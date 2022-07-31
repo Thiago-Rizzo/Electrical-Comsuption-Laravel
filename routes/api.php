ed4aee740dce799171c9b8518724e3a411de7ded
Alvaro Tedesco <alv.ted@outlook.com>
2022-07-31 17:06:20 -0300

    Create routes to controllers

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Login routes
+Route::post('auth/login', '\App\Http\Controllers\AuthController@login');
+
+// Container routes
+Route::get('index-container/{user_id}', '\App\Http\Controllers\ContainerController@index');
+Route::get('show-container/{container_id}', '\App\Http\Controllers\ContainerController@show');
+Route::post('store-container', '\App\Http\Controllers\ContainerController@store');
+Route::put('update-container/{container_id}', '\App\Http\Controllers\ContainerController@update');
+Route::delete('delete-container/{container_id}', '\App\Http\Controllers\ContainerController@delete');
+Route::delete('delete-device-container/{container_id}/{device_id}', '\App\Http\Controllers\ContainerController@deleteDeviceController');
+
+// Device routes
+Route::get('index-device/{user_id}','\App\Http\Controllers\DeviceController@index');
+Route::get('show-device/{device_id}','\App\Http\Controllers\DeviceController@show');
+Route::post('store-device','\App\Http\Controllers\DeviceController@store');
+Route::put('update-device/{device_id}','\App\Http\Controllers\DeviceController@update');
+Route::delete('delete-device/{device_id}','\App\Http\Controllers\DeviceController@delete');
+
+// User routes
+Route::get('index-user/{user_id}', '\App\Http\Controllers\UserController@index');
+Route::get('show-user/{user_id}', '\App\Http\Controllers\UserController@show');
+Route::post('store-user', '\App\Http\Controllers\UserController@store');
+Route::put('update-user/{user_id}', '\App\Http\Controllers\UserController@update');
+Route::delete('delete-user/{user_id}', '\App\Http\Controllers\UserController@delete');
