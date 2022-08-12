13db2fff1d4eb2032117564839f77bb6ebb0487a
Controler Thiago Rizzo <controler02@coverpay.com.br>
2022-08-12 15:06:51 -0300

    d

--- app/Http/Controllers/ContainerController.php
+++ app/Http/Controllers/ContainerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Container;
+use App\Models\ContainerDevice;
 use Illuminate\Support\Facades\DB;
 
 class ContainerController extends Controller
